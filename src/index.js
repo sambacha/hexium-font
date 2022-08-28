45ea64678b87d0ede80b2fc63515bee9c2d4a636
sam bacha <sam@manifoldfinance.com>
2022-08-27 22:51:58 -0700

    bundle

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Ethereum Hex Font</h1>
+<br>
+<br>
+<pre>
+0x44532e0c322c0e3f7aff9180e872ff52fbe3538d4a26fd9829209d623d7dce17
+</pre>
+<br>
+<h3> 0x56650833ddef615fddd2b3082b06438fa050a6b8dce83151df1c2a31b7828bd8 </h3>
+<br>
+<p>0x4e4fb994039591f4c65c47e19629052a80943ab4226f7aec730cde5831a10672</p>
+
+<pre>Checksum</pre>
+✔︎ true
+<p>0x27b1fdb04752bbc536007a920d24acb045561c26</p>
+
+<pre>Checksum ChainId</pre>
+✔︎ true
+<p>0x27b1FdB04752BBc536007A920D24ACB045561c26</p>
+
+
+
+
+`;
