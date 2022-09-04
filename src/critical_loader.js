14eb742daac4bf767d5623382424404428b04222
s bacha <sam@manifoldfinance.com>
2022-09-04 02:28:58 -0700

    Critical FOFT with preload

---
+++ src/critical_loader.js
@@ -0,0 +1,25 @@
+/* Font loading: Critical FOFT with preload, see: https://www.zachleat.com/web/comprehensive-webfonts/#critical-foft-with-preload */
+(function() {
+  if ('fonts' in document) {
+    /* Optimization for Repeat Views */
+    if (sessionStorage.fontsLoadedCriticalFoftPreload) {
+      document.documentElement.className += ' fonts-loaded-2';
+      return;
+    }
+    document.fonts.load("1em 'Scancardium3'")
+      .then(function() {
+        document.documentElement.className += ' fonts-loaded-1';
+        Promise.all([
+        document.fonts.load("400 1em 'Scancardium3'"),
+        document.fonts.load("700 1em 'Scancardium3'"),
+        document.fonts.load("italic 1em 'Scancardium3'"),
+        document.fonts.load("italic 700 1em 'Scancardium3'"),
+      ])
+          .then(function() {
+            document.documentElement.className += ' fonts-loaded-2';
+            /* Optimization for Repeat Views */
+            sessionStorage.fontsLoadedCriticalFoftPreload = true;
+          });
+      });
+  }
+})();
